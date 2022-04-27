26c3291ec69ebdd5a44a4e5237829151b4b2c2ae
Korotaev Nikita <nalkorotaev@sberbank.ru>
2022-04-27 20:15:02 +0300

    feat: add logic to save report

--- report/admin_site/report_admin.py
+++ report/admin_site/report_admin.py
@@ -1,4 +1,6 @@
 from django.contrib.admin import ModelAdmin
+from django.db.models import Sum, Q
+from dds.models import Operation
 
 
 class ReportAdmin(ModelAdmin):
@@ -8,3 +10,20 @@ class ReportAdmin(ModelAdmin):
     list_filter = ('user',)
     search_fields = ('name',)
     filter_horizontal = ('articles',)
+
+    def save_model(self, request, obj, form, change):
+        operations = Operation.objects.values('amount').filter(
+            date__gte=form.cleaned_data['start_date'],
+            date__lte=form.cleaned_data['end_date'],
+            user=request.user,
+            article__in=form.cleaned_data['articles'],
+        ).annotate(
+            plus=Sum('amount', filter=Q(is_purchase=False))
+        ).annotate(
+            minus=Sum('amount', filter=Q(is_purchase=True))
+        ).aggregate(
+            income=Sum('plus'), expenses=Sum('minus'))
+        obj.total = (operations.get('income') or 0) - (operations.get('expenses') or 0)
+        obj.income = operations.get('income', 0) or 0
+        obj.expenses = operations.get('expenses', 0) or 0
+        obj.save()
