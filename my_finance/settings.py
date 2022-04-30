f54a1ec325f7cf4edc56137a218bb9e028dd1124
Korotaev Nikita <nalkorotaev@sberbank.ru>
2022-04-30 14:32:54 +0300

    ref: add setting for static

--- my_finance/settings.py
+++ my_finance/settings.py
@@ -132,6 +132,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
