88ad73d15e4a8f920f6026a4afd48059a28a29e5
Korotaev Nikita <nalkorotaev@sberbank.ru>
2022-04-30 13:54:34 +0300

    fix: fix path to bd

--- my_finance/settings.py
+++ my_finance/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'my_finance.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
         'CONN_MAX_AGE': 20,
     }
 }
